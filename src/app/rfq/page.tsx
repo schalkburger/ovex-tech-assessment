5082d1d56dfd9ff68bde4b17c29d38dd583ca176
Schalk Burger <schalkb@gmail.com>
2024-11-24 18:51:45 +0200

    Initial fetchMarkets function

--- src/app/rfq/page.tsx
+++ src/app/rfq/page.tsx
@@ -1,5 +1,8 @@
 // app/rfq/page.tsx
 "use client";
+
+import { useEffect } from "react";
+
 interface Market {
   id: string;
   name: string;
@@ -20,9 +23,24 @@ interface Quote {
 }
 
 export default function RFQPage() {
+  useEffect(() => {
+    const fetchMarkets = async () => {
+      try {
+        const response = await fetch("/api/markets");
+        const data = await response.json();
+        // setMarkets(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching markets:", error);
+      }
+    };
+
+    fetchMarkets();
+  }, []);
+
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Request-for-Quote (RFQ)</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-mirage-950">
+      <h1 className="text-3xl font-bold mb-4 text-white">Request for Quote</h1>
     </div>
   );
 }
