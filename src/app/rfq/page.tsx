408a0b4375e99e9570b2fe2f2300488721a53d51
Schalk Burger <schalkb@gmail.com>
2024-11-24 18:28:43 +0200

    Setup initial /rfq page

---
+++ src/app/rfq/page.tsx
@@ -0,0 +1,10 @@
+// app/rfq/page.tsx
+"use client";
+
+export default function RFQPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Request-for-Quote (RFQ)</h1>
+    </div>
+  );
+}
