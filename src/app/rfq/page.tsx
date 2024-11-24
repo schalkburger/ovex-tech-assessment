836b515d8c1cf18142a10f4c4fd064f52264d344
Schalk Burger <schalkb@gmail.com>
2024-11-24 18:29:24 +0200

    Setup types/interfaces

--- src/app/rfq/page.tsx
+++ src/app/rfq/page.tsx
@@ -1,5 +1,23 @@
 // app/rfq/page.tsx
 "use client";
+interface Market {
+  id: string;
+  name: string;
+  base_currency: string;
+  quote_currency: string;
+  base_precision: number;
+  quote_precision: number;
+  rfq_enabled: boolean;
+  indicative_pricing: boolean;
+  base_currency_long: string;
+  quote_currency_long: string;
+}
+
+interface Quote {
+  cost: number;
+  rate: number;
+  total_amount: number;
+}
 
 export default function RFQPage() {
   return (
